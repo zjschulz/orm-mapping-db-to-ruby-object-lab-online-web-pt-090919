11488d457a8d9ff968018acec5e17355d612df49
Zachary Schulz <zjschulz@gmail.com>
2019-11-15 02:19:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,4 +86,14 @@ class Student
     end.first(x)
   end
   
+  def self.first_students_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
 end
