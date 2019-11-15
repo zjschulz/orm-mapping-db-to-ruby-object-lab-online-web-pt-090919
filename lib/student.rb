31860e26ebd6cac1e1c0b6520370fc20f7e26756
Zachary Schulz <zjschulz@gmail.com>
2019-11-15 02:15:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ class Student
       SELECT * FROM students WHERE grade = 10 LIMIT x
     SQL
  
-    DB[:conn].execute(sql, x).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
