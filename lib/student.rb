d84d368e35a8a466f32d3de6ca99833ebf07acd1
Zachary Schulz <zjschulz@gmail.com>
2019-11-15 02:20:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
     end.first(x)
   end
   
-  def self.first_students_in_grade_10
+  def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10
     SQL
