6247ead8bdabb49c7eb311a42b53d3257261b8dd
Zachary Schulz <zjschulz@gmail.com>
2019-11-15 02:21:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,7 +101,7 @@ class Student
       SELECT * FROM students WHERE grade = ?
     SQL
  
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
