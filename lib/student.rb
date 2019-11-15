371d34a5a96f7ede916da22809490739cb9c4137
Zachary Schulz <zjschulz@gmail.com>
2019-11-15 02:08:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,9 +57,8 @@ class Student
   end
   
   def self.all_students_in_grade_9
-  sql = <<-SQL
-      SELECT *
-      FROM students
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
     SQL
  
     DB[:conn].execute(sql).map do |row|
