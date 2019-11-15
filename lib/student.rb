fd48a066b7c3988c3bc51bccdbb6d46e37784703
Zachary Schulz <zjschulz@gmail.com>
2019-11-15 02:13:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,4 +76,14 @@ class Student
     end
   end
   
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
 end
